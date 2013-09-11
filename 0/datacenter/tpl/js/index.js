6dbf779a9e31b785684f3ccddb023389bd6452f1
cubika <receiversume@163.com>
2013-09-11 13:21:30 +0800

    youhua version 0.5

--- 0/datacenter/tpl/js/index.js
+++ 0/datacenter/tpl/js/index.js
@@ -1,22 +1,28 @@
-(function($){
 
-	$('.fancybox').fancybox();
-	$('.chosen-select').chosen();
-	$('.chosen-select-deselect').chosen({
-		allow_single_deselect : true
+(function($) {
+	
+	$.ajaxSetup({
+		timeout:5000,
+		error:function(){
+			$("#timeout").modal('show');
+		}
 	});
+
+	$(document).ajaxStart(function() {
+		$("body").addClass("loading");
+	}).ajaxStop(function() {
+		$("body").removeClass("loading");
+	});
+
 	$("#tab1").load("ldt_query.php");
 	$("#tab4").load("ldt_eva.php");
 
-
-	$(".sidebar-nav a").on("click",function(){
-		$(".sidebar-nav li.active").removeClass("active");
-		var li=$(this).closet("li");
-		$(li).addClass("active");
-		var module=$(li).data("module");
-		if(module !== "eva"){
-			$("#tab1").load(module+"_query.php");
-			$("#tab4").load(module+"_eva.php");
-		}
+	$(".performance a").on("click", function() {
+		var $this=$(this);
+		$(".performance a.active").removeClass("active");
+		$this.addClass("active");
+		var module = $this.data("module");
+		$("#tab1").load(module + "_query.php");
+		$("#tab4").load(module + "_eva.php");
 	});
 })(jQuery);
